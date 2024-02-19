07546a26d563db5799f6d77d77a1a7f47c60d144
Epsilonator <somichat@gmail.com>
2024-02-19 13:36:46 +0530

    Added IntQ function

--- src/main/java/io/mathlark/numbers/NumberFuncsRegistry.java
+++ src/main/java/io/mathlark/numbers/NumberFuncsRegistry.java
@@ -2,11 +2,13 @@ package io.mathlark.numbers;
 
 import io.mathlark.funcs.FunctionRegistryContract;
 import io.mathlark.numbers.funcs.EvenQFunc;
+import io.mathlark.numbers.funcs.IntQFunc;
 import io.mathlark.numbers.funcs.NumQFunc;
 
 public class NumberFuncsRegistry extends FunctionRegistryContract  {
     public NumberFuncsRegistry() {
         register(EvenQFunc.class);
         register(NumQFunc.class);
+        register(IntQFunc.class);
     }
 }
---
+++ src/main/java/io/mathlark/numbers/funcs/IntQFunc.java
@@ -0,0 +1,47 @@
+package io.mathlark.numbers.funcs;
+
+import io.mathlark.exceptions.WrongParameterLengthException;
+import io.mathlark.exceptions.WrongParameterTypeException;
+import io.mathlark.funcs.CallableUtils;
+import io.mathlark.funcs.LarkCallable;
+import io.mathlark.parser.GlobalSymbolRegistry;
+import io.mathlark.parser.expressions.BooleanExpression;
+import io.mathlark.parser.expressions.IExpression;
+import io.mathlark.parser.expressions.NumericExpression;
+
+public class IntQFunc implements LarkCallable {
+
+    @Override
+    public IExpression evaluate(IExpression... expressions) {
+        try {
+            checkArgs(expressions);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return GlobalSymbolRegistry.UNDEFINED;
+        }
+
+        if (CallableUtils.isInstanceOf(expressions[0], NumericExpression.class)) {
+            Number val = ((NumericExpression) expressions[0]).getVal();
+            if (val instanceof Integer) {
+                return new BooleanExpression(true);
+            }
+        }
+
+        return new BooleanExpression(false);
+    }
+
+    @Override
+    public void checkArgs(IExpression... expressions)
+            throws WrongParameterLengthException, WrongParameterTypeException {
+        if (expressions.length != 1) {
+            throw new WrongParameterLengthException("Expected 1 element, got %d elements", expressions.length);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "IntQ";
+    }
+    
+}
